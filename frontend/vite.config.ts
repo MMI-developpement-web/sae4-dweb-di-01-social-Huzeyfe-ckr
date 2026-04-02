1335111a35ad136d912c566313e4cb9208447c09
Macbook air M4 de Huzeyfe <macbookairm4dehuzeyfe@wifi-eduroam-etud-226-239.unilim.fr>
2026-04-02 10:31:07 +0200

    🚀 Deploy: CycleD configuration for MMI hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/~cakir4/CycleD/frontend/",
   plugins: [react(), tailwindcss()],
   preview: {
    port: 5173,
